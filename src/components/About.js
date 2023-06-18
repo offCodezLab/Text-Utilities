4594d3f72eda5332477355adabe2dec488190d56
offCodezLab <official.codeshub@gmail.com>
2023-06-18 08:08:28 +0500

    Modified App.css and About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,7 @@ export default function About(props) {
         <>
             <div className='container'>
                 <h1 className='my-5' style={{ color: props.mode === "light" ? "black" : "white", textAlign: "center" }}>About Us</h1>
+                <h5 className='my-5' style={{ color: props.mode === "light" ? "black" : "white", textAlign: "center" }}>Welcome to Text Utilities. Analyze your text with our single page appliaction!</h5>
                 <div className="accordion" id="accordionExample">
                     <div className="accordion-item">
                         <h2 className="accordion-header">
@@ -16,7 +17,7 @@ export default function About(props) {
                         </h2>
                         <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
                             <div className={`accordion-body ${"about-" + props.mode}`}>
-                                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                <strong>Text Utilities</strong> is a single page appliaction that let you analyze your text in no time. You can perform various actions of your choice on text and the copy them to your clipboard.
                             </div>
                         </div>
                     </div>
@@ -28,7 +29,7 @@ export default function About(props) {
                         </h2>
                         <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                             <div className={`accordion-body ${"about-" + props.mode}`}>
-                                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                <strong>Text Utilities</strong> is a free to use web application and have no special kind of restrictions.
                             </div>
                         </div>
                     </div>
@@ -40,7 +41,7 @@ export default function About(props) {
                         </h2>
                         <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                             <div className={`accordion-body ${"about-" + props.mode}`}>
-                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                <strong>Text Utilities</strong> is not only free to use but also a fast and lightweight way to analyze text.
                             </div>
                         </div>
                     </div>
